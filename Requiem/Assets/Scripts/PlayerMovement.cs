da73e5a16ba4b0a81afaf7dcea8ce8ad305c84a9
Shifty332 <spolaor.stefano@gmail.com>
2026-04-10 20:47:39 +0200

    Implementazione funzioni di movimento

--- Requiem/Assets/Scripts/PlayerMovement.cs
+++ Requiem/Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDir;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputManagement();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY);
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
     }
 }
